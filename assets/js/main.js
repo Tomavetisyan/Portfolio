3c6c695d9240c8caadc671af7fd158b3c1466486
Tomavetisyan <54915660+Tomavetisyan@users.noreply.github.com>
2020-05-26 16:10:42 -0700

    Slideshow and skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,17 @@ function myFunction(id) {
 	  x.style.display = "none";
 	}
   }	
+
+  $("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+}, 5000);
 (function($) {
 
 	var $window = $(window),
