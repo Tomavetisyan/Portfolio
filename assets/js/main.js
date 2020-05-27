7f1f0a50693d6d85e8e05a8d1c57ad80a7ffc4c0
Tomavetisyan <54915660+Tomavetisyan@users.noreply.github.com>
2020-05-27 00:50:46 -0700

    navbar and dark theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,15 +3,7 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
-var slideIndex = 0;
-function myFunction(id) {
-	var x = document.getElementById(id);
-	if (x.style.display === "none") {
-	  x.style.display = "block";
-	} else {
-	  x.style.display = "none";
-	}
-  }	
+
 
   $(".slideshow div:gt(0)").hide();
 
